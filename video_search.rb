3411fedf4ddd421e0e761a99e0967c54a5b92017
ValentinPerret <valentin.p.perret@gmail.com>
2015-10-21 15:30:25 -0700

    debug omx player

--- video_search.rb
+++ video_search.rb
@@ -4,5 +4,6 @@ puts 'Hello-World'
 puts "What's the video name"
 code = gets.chomp
 video = `youtube-dl -g #{code}`
+puts video + " attention c'est parti"
 
-system("omxplayer `#{video}`")
+system("omxplayer -b #{video}`")
