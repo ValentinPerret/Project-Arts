9b4888adfb602168c11d76eefc5991e40b44e861
ValentinPerret <valentin.p.perret@gmail.com>
2015-10-21 16:04:49 -0700

    Deux videos

--- video_search.rb
+++ video_search.rb
@@ -4,5 +4,8 @@ puts 'Hello-World'
 puts "What's the video name"
 code = gets.chomp
 code = '8jZ3h6lbjDw'
+code2 = '_NWmuRZm85E'
 
 system("omxplayer -b `youtube-dl -g #{code}`")
+
+system("omxplayer -b `youtube-dl -g #{code2}`")
